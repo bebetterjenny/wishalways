690c5cfd8d1103fcf30955acc93e9bae5d545e43
Jenny Han <bebetterjenny@gmail.com>
2018-06-14 23:08:35 -0400

    create new theme

---
+++ app/design/frontend/btr/wish/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © btr, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/btr/wish',
+    __DIR__
+);
